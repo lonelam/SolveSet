673c58c4037fde16ca770c1de506039aafc58a05
lonelam <laizenan@gmail.com>
2016-12-06 01:27:05 +0800

    add an author changer

---
+++ modify_author.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_USERNAME="lonelam"
+CORRECT_NAME="lonelam"
+CORRECT_EMAIL="laizenan@gmail.com"
+if [ "$GIT_COMMITTER_NAME" = "$OLD_USERNAME" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_NAME" = "$OLD_USERNAME" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
