6b542a02be720d1f2a14d7c5538f1e04e96ad541
Lonelam <laizenan@gmail.com>
2016-12-15 16:49:14 +0800

    modify CF-problem-E

--- cf743E/cf743E/OJtemplate.cpp
+++ cf743E/cf743E/OJtemplate.cpp
@@ -17,86 +17,92 @@ bool vis[10];
 int a[1000 + 10];
 int n;
 int ans;
-bool dfs(int cur, int x, int p, int ccc, int cl = -1)
+bool dfs(int rem, int x, int p, int ccc, int cl = -1)
 {
-	if (cur == 0)
-	{
-		ans = max(ans, ccc);
-		return true;
-	}
-	int cnt = 0;
-	if (cl != -1)
-	{
-		for (; p < n && cnt < x; p++)
-		{
-			if (a[p] == cl)
-				cnt++;
-		}
-		if (cnt != x)
-		{
-			return false;
-		}
-	}
-	ccc += cnt;
-	bool fret = false;
-	for (int i = 1; i <= 8; i++)
-	{
-		if (!vis[i])
-		{
-			vis[i] = true;
-			if (dfs(cur - 1, x, p, ccc, i))
-				fret = true;
-			vis[i] = false;
-		}
-	}
-	if (cl != -1)
-	{
-		for (; p < n && cnt < x+1; p++)
-		{
-			if (a[p] == cl)
-				cnt++;
-		}
-		if (cnt != x + 1)
-			return fret;
-	}
-	ccc++;
-	for (int i = 1; i <= 8; i++)
-	{
-		if (!vis[i])
-		{
-			vis[i] = true;
-			if (dfs(cur - 1, x, p, ccc, i))
-				fret = true;
-			vis[i] = false;
-		}
-	}
-	return fret;
+    int cnt = 0;
+    bool xok = false;
+    if (cl != -1)
+    {
+        for (; p < n && cnt < x; p++)
+        {
+            if (a[p] == cl)
+            {
+                cnt++;
+            }
+        }
+        if (cnt != x)
+        {
+            return false;
+        }
+        if (rem == 0)
+        {
+            ans = max(ans, ccc);
+            xok = true;
+        }
+    }
+
+    for (int i = 1; i <= 8; i++)
+    {
+        if (!vis[i])
+        {
+            vis[i] = true;
+       //     cout << "push\t"<< i <<endl;
+            xok = dfs(rem - 1, x, p, ccc + cnt, i) || xok;
+
+            vis[i] = false;
+        }
+    }
+    if (cl == -1)
+        return xok;
+    for (; p < n && cnt < x + 1; p++)
+    {
+        if (a[p] == cl)
+            cnt++;
+    }
+    if (cnt != x +1) return xok;
+    if (rem == 0)
+    {
+        ans = max(ans, cnt + ccc);
+        return true;
+    }
+    for (int i = 1; i <= 8; i++)
+    {
+        if (!vis[i])
+        {
+            vis[i] = true;
+            xok = dfs(rem - 1, x, p, ccc + cnt, i) || xok;
+            vis[i] = false;
+        }
+    }
+    return xok;
 }
 int main()
 {
-	while (cin >> n)
-	{
-		for (int i = 0; i < n; i++)
-		{
-			cin >> a[i];
-		}
-		int l = 1, r = n;
-		ans = 0;
-		memset(vis, 0, sizeof(vis));
-		while (l + 1 < r)
-		{
-			const int mid = l + (r - l) / 2;
-			if (dfs(8, mid, 0, 0))
-			{
-				l = mid;
-			}
-			else
-			{
-				r = mid;
-			}
-		}
-		dfs(8, l, 0, 0);
-		cout << ans << endl;
-	}
+    ios::sync_with_stdio(false);
+    while(cin >> n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        ans = 0;
+        memset(vis, 0, sizeof(vis));
+        int l = 0, r = n / 8 + 2;
+        while(l + 1 < r)
+        {
+            const int mid = l + (r - l)/ 2;
+            if (dfs(8, mid, 0, 0))
+            {
+                l = mid;
+            }
+            else
+            {
+                r = mid;
+            }
+        }
+        dfs(8, l, 0, 0);
+        if (l != 0) dfs(8, l - 1, 0, 0);
+        cout << ans <<endl;
+    }
 	return 0;
 }
