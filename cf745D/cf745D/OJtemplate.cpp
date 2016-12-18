75b26ec113622f321cdc4e3ff181833632f8c118
Lonelam <laizenan@gmail.com>
2016-12-18 12:55:03 +0800

    Daily problems commit, 12/18/2016 Sun

---
+++ cf745D/cf745D/OJtemplate.cpp
@@ -0,0 +1,24 @@
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<queue>
+#include<map>
+#include<string>
+#include<cmath>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main()
+{
+
+	int n;
+	scanf("%d", &n);
+
+	return 0;
+}
