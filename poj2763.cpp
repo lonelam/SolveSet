563fbce0a86223259b2269dfe3641af24ef8f4cd
Lonelam <laizenan@gmail.com>
2016-12-27 03:20:51 +0800

    modified:   poj2763.cpp

--- poj2763.cpp
+++ poj2763.cpp
@@ -1,93 +1,24 @@
-#include<cstdio>
-#include<algorithm>
-#include<cstring>
+#include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
-struct edge {int id, to, cost;};
-const int maxn = 100001 + 20;
-const int maxq = maxn;
+struct edge
+{
+    int id, to, cost;
+};
 int n, q, s;
-int a[maxn - 1], b[maxn - 1], w[maxn - 1];
-int type[maxq];
-int x[maxq], t[maxq];
+vector<edge> es;
+int a[maxn], b[maxn], w[maxn];
 
-vector<edge> G[maxn];
-// bit here
-
-int bit_n;
+vector<int> G[maxn];
 
 int root;
-
 int vs[maxn * 2 - 1];
 int depth[maxn * 2 - 1];
 int id[maxn];
-int es[ (maxn - 1) * 2];
-void dfs(int v, int p, int d, int & k)
+int es[]
+void dfs(int u, int p, int d)
 {
-	id[v] = k;
-	vs[k] = v;
-	depth[k++] = d;
-	for (int i = 0; i < G[v].size(); i++)
-	{
-		edge & e = G[v][i];
-		if (e.to != p)
-		{
-			add(k, e.cost);
-			es[e.id * 2] = k;
-			dfs(e.to, v,d + 1, k);
-			vs[k] = v;
-			depth[k++] = d;
-			add(k, -e.cost);
-			es[e.id * 2 + 1] = k;
-		}
-	}
+
 }
-
-int stack_v[maxn +10];
-int stack_i[maxn + 10];
-
-void init(int V)
-{
-	bit_n = (V - 1) * 2;
-	int k = 0;
-	dfs(root, -1, 0, k);
-	rmq_init(depth, V * 2 - 1);
-}
-
-int lca(int u, int v)
-{
-	return vs[query(min(id[u], id[v]), max(id[u], id[v])];
-}
-
-
-void solve()
-{
-	root = n / 2;
-	for (int i = 0; i < n - 1; i++)
-	{
-		G[a[i] - 1].push_back((edge){i, b[i] - 1, w[i]});
-		G[b[i] - 1].push_back((edge){i, a[i] - 1, w[i]});
-		init(n);
-		for (int i = 0; i < q; i++)
-		{
-			if (type[i] == 0)
-			{
-				int u = x[i] - 1;
-				int p = lca(v, u);
-				printf("%d\n", sum(id[v]) + sum(id[u]) - sum(id[p]) * 2);
-				v = u;
-			}
-			else
-			{
-				int k = x[i] - 1;
-				add(es[k *2], t[i] - w[k]);
-				add(es[k * 2 + 1], w[k] - t[i]);
-				w[k] = t[i];
-			}
-		}
-	}
-}
-
-
-
--- poj3709/poj3709/Source.cpp
+++ poj3709/poj3709/Source.cpp
@@ -1,16 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<cstdio>
-#include<cstring>
 #include<iostream>
+#include<cstring>
+#include<cstdio>
 using namespace std;
-
-const int maxn = 500000 + 100;
-int n, k;
 typedef long long ll;
+int n, k;
+const int maxn = 500000 + 100;
 ll a[maxn];
-ll dp[maxn];
+ll dp[maxn + 1];
 ll S[maxn];
-ll deq[maxn];
+int deq[maxn];
 
 ll f(int j, int x)
 {
@@ -22,7 +21,7 @@ bool check(int f1, int f2, int f3)
 	ll a1 = -a[f1], b1 = dp[f1] - S[f1] + a[f1] * f1;
 	ll a2 = -a[f2], b2 = dp[f2] - S[f2] + a[f2] * f2;
 	ll a3 = -a[f3], b3 = dp[f3] - S[f3] + a[f3] * f3;
-	return (a2 - a1) *(b3 - b2) >= (b2 - b1) * (a3 - a2);
+	return (a2 - a1) * (b3 - b2) >= (b2 - b1) * (a3 - a2);
 }
 
 void solve()
@@ -32,7 +31,8 @@ void solve()
 		S[i + 1] = S[i] + a[i];
 	}
 	int s = 0, t = 1;
-	deq[0] = dp[0] = 0;
+	deq[0] = 0;
+	dp[0] = 0;
 	for (int i = k; i <= n; i++)
 	{
 		if (i - k >= k)
@@ -40,7 +40,7 @@ void solve()
 			while (s + 1 < t && check(deq[t - 2], deq[t - 1], i - k)) t--;
 			deq[t++] = i - k;
 		}
-		while (s + 1 < t && f(deq[s], i) >= f(deq[s + 1], i)) s++;
+		while (s + 1 < t && f(deq[s], i) >= f(deq[s + 1], i))s++;
 		dp[i] = S[i] + f(deq[s], i);
 	}
 	printf("%lld\n", dp[n]);
@@ -50,14 +50,6 @@ int main()
 	int T;
 	while (scanf("%d", &T) != EOF)
 	{
-		while (T--)
-		{
-			scanf("%d%d", &n, &k);
-			for (int i = 0; i < n; i++)
-			{
-				scanf("%lld", a + i);
-			}
-			solve();
-		}
+		while(T--)
 	}
 }
