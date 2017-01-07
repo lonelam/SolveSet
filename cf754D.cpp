95d28f1c6e999049492ac9832ed7220a93764822
lonelam <laizenan@gmail.com>
2017-01-08 00:55:31 +0800

    Daily commit Sun Jan  8 00:55:31 CST 2017

--- cf754D.cpp
+++ cf754D.cpp
@@ -4,11 +4,96 @@ typedef long long ll;
 typedef long double ld;
 const int inf = 0x3f3f3f3f;
 const int maxn = 300000 + 1000;
-int li[maxn], ri[maxn];
-int cnt[maxn * 4];
-int disc[maxn * 2];
+const ll moder = 1e9+7;
+ll li[maxn], ri[maxn];
+int lazy[maxn * 4];
+//int cnt[maxn * 4];
+ll disc[maxn * 2];
 int n, k;
 int dn;
+
+void build(int l = 0, int r = dn, int rt = 1)
+{
+    if (l >= r)
+    {
+        return;
+    }
+    lazy[rt] = 0;
+    if (l + 1 == r)
+    {
+        return;
+    }
+    const int mid = (l + r) /2 ;
+    build(l, mid, rt << 1);
+    build(mid, r, rt <<1 | 1);
+}
+void pushdown(int rt)
+{
+    if (lazy[rt] == 0)
+    {
+        return;
+    }
+    lazy[rt << 1] += lazy[rt];
+    lazy[rt <<1 | 1] += lazy[rt];
+    lazy[rt] = 0;
+}
+void add(int L, int R, int l = 0, int r= dn, int rt = 1)
+{
+    if (l >= r)
+    {
+        return;
+    }
+    if (L <= l && r <= R)
+    {
+    //    cout << l <<" "<< r << endl;
+        lazy[rt]++;
+        return;
+    }
+    if (l + 1 == r)
+    {
+        return;
+    }
+    pushdown(rt);
+    const int mid = (l + r) / 2;
+    if (L < mid)
+    {
+        add(L, R, l, mid, rt << 1);
+    }
+    if (mid <= R)
+    {
+        add(L,R,mid, r, rt << 1 | 1);
+    }
+}
+void alldown(int l = 0, int r = dn, int rt = 1)
+{
+    if (l + 1 == r)
+    {
+    //    cout << lazy[rt] << " ";
+        return;
+    }
+    pushdown(rt);
+    const int mid = (l + r) / 2;
+    alldown(l, mid, rt << 1 );
+    alldown(mid, r, rt << 1 | 1);
+}
+int query(int q, int l = 0, int r = dn, int rt = 1)
+{
+    if (l + 1 == r)
+    {
+        //if (l == q)
+        return lazy[rt];
+        //else return -1;
+    }
+    const int mid = (l + r) / 2;
+    if (q < mid)
+    {
+        return query(q,l, mid, rt << 1);
+    }
+    else
+    {
+        return query(q,mid, r, rt << 1 | 1);
+    }
+}
 void init()
 {
     dn = 0;
@@ -19,60 +104,38 @@ void init()
     }
     sort(disc, disc + dn);
     dn = unique(disc, disc + dn) - disc;
-    memset(cnt, 0, sizeof(cnt));
-}
-int lsb(int x)
-{
-    return x & (-x);
-}
-void add(int x, int val)
-{
-    x++;
-    while(x <= dn)
-    {
-        cnt[x] += val;
-        x += lsb(x);
-    }
-}
-
-int sum(int x)
-{
-    x++;
-    int ret = 0;
-    while(x > 0)
-    {
-        ret += cnt[x];
-        x -= lsb(x);
-    }
-    return ret;
+    build();
+    //memset(cnt, 0, sizeof(cnt));
 }
 void solve()
 {
-    cout << "degb";
+//    cout << "degb";
     init();
-    cout <<"deb";
+//    cout <<"deb";
     for (int i = 0; i < n;i ++)
     {
         int lpos = lower_bound(disc, disc + dn, li[i] * 2 + 1) - disc;
         int rpos = lower_bound(disc, disc + dn, ri[i] * 2  ) - disc;
-        add(rpos, 1);
-        add(lpos - 1, -1);
+    //    cout << lpos <<" "<< rpos << endl;
+        add(lpos, rpos );
     }
-    int pre = 0;
-    int ansL = 0;
-    int ansR = 0;
-    int ans = 0;
+    alldown();
+    ll pre = 0;
+    ll ansL = 0;
+    ll ansR = 0;
+    ll ans = 0;
     for (int i = 0; i < dn; i++)
     {
-        int tt = sum(i) - sum(i - 1);
+        int tt = query(i);
+        cout <<disc[i] << " "<< tt <<"  "<< pre << "  "<<i <<endl;
         if (tt < k && pre != i )
         {
-            int len = disc[i-1] / 2 - disc[pre] / 2+1;
+            ll len = disc[i] / 2LL - disc[pre] / 2LL;
             if (len > ans)
             {
                 ans = len;
-                ansL = disc[pre] / 2;
-                ansR = disc[i - 1] / 2;
+                ansL = disc[pre] / 2LL;
+                ansR = disc[i] / 2LL;
             }
         }
         if (tt < k)
@@ -80,9 +143,11 @@ void solve()
             pre = i+1;
         }
     }
+
     if (pre != dn)
     {
-        int len = disc[dn - 1] / 2 - disc[pre] / 2 + 1;
+        cout << "error";
+        int len = disc[dn - 1] / 2 - disc[pre] / 2 ;
         if (len > ans)
         {
             ans = len;
@@ -90,6 +155,7 @@ void solve()
             ansR = disc[dn - 1] / 2;
         }
     }
+    //cout << "deg";
     if (ans == 0)
     {
         printf("0\n");
@@ -101,7 +167,9 @@ void solve()
     }
     else
     {
-        for (int i = 0; i < n; i++)
+        cout << ansL <<" "<<ansR << endl;
+        printf("%d\n", ans);
+        for (int i = 0;k && i < n; i++)
         {
             if (li[i] <= ansL && ansR <= ri[i])
             {
@@ -110,6 +178,7 @@ void solve()
             }
         }
     }
+//    cout <<"wegtj";
 }
 int main()
 {
@@ -119,11 +188,13 @@ int main()
         for (int j = 0; j < n; j++)
         {
         //    cout <<"debug";
-        cout << n << endl;
-        scanf("%d%d", li + j, ri+j);
+    //    cout << n << endl;
+        scanf("%lld%lld", li + j, ri+j);
+        li[j] += moder;
+        ri[j] += moder + 1LL;
             //cin >> li[j] >> ri[j];
         }
-        cout << "deubg";
+//        cout << "deubg";
         solve();
     }
 }
