ca03e7434af4105c3303f88ee044cc1d3024fd31
lonelam <laizenan@gmail.com>
2017-06-18 21:51:03 +0800

    Daily commit Sun Jun 18 21:51:03 CST 2017

---
+++ 17codemA2.cpp
@@ -0,0 +1,21 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const int inf = 0x3f3f3f3f;
+const int maxn = 51;
+int a[maxn];
+int b[maxn]
+bool dfs()
+{
+
+}
+int main()
+{
+  int T;
+  while(scanf("%d", &T) != EOF)
+  {
+    
+  }
+}
