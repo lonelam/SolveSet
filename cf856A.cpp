a1535a6049a9d4cd729b4718c2a5a6c494e0ed5c
Lonelam <laizenan@gmail.com>
2017-09-13 12:50:05 +0800

    Daily commit 2017年09月13日 12:50:04

---
+++ cf856A.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const int inf = 0x3f3f3f3f;
+const int maxn = 1000000 + 100;
+set<int> B;
+set<int> A;
+int main()
+{
+  int T;
+  int n;
+  scanf("%d", &T);
+  while(T--)
+  {
+    scanf("%d", &n);
+    A.clear();
+    B.clear();
+    for (int i = 0; i < n; i++)
+    {
+      int tmp;
+      scanf("%d", &tmp);
+      A.insert(tmp);
+    }
+  }
+}
